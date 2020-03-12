35f1c75015ca0410438e25433e601922e510d9e5
yodev21 <yodev2177@gmail.com>
2020-03-12 20:22:24 +0900

    余分な空白を削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'whispering-garden-78309.herokuapp.com ' }
+  config.action_mailer.default_url_options = { host: 'whispering-garden-78309.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
