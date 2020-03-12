860bc5b7fd49f3b325c89313e18929bf3aad84c1
yodev21 <yodev2177@gmail.com>
2020-03-12 13:33:49 +0900

    heroku再設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-    config.action_mailer.default_url_options = { host: 'desolate-beach-29305.herokuapp.com' }
+    config.action_mailer.default_url_options = { host: 'mysterious-cliffs-70241.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
